81bacaa3a19918707058c2c7e6a2ded045f27a6c
Kenny Chiu <kennychiu@jourdeness.com.tw>
2016-07-07 16:33:29 +0800

    fix deadline bug

--- app/helpers/progresses_helper.rb
+++ app/helpers/progresses_helper.rb
@@ -9,12 +9,11 @@ module ProgressesHelper
   end
   def render_deadline_format(progress)
     if ENV['RACK_ENV'] == 'production' then
-      #date_str = progress.deadline
-      #date_str.slice! "{"
-      #date_str.slice! "}"
-      #date_hash = Hash[date_hash.split(",").collect{|x| x.strip.split("=>")}]
-      #deadline_format = "#{date_hash['1']}/#{date_hash['2']}/#{date_hash['3']}"
-      progress.deadline
+      date_str = progress.deadline
+      date_str.slice! "{"
+      date_str.slice! "}"
+      date_hash = Hash[date_str.split(",").collect{|x| x.strip.split("=>")}]
+      deadline_format = "#{date_hash['1']}/#{date_hash['2']}/#{date_hash['3']}"      
     else
       progress.deadline
     end
